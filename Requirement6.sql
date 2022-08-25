2cf5763dd8f95e904aef8d92f442118636fedb48
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-25 13:09:59 +0300

    Modidy fnCompanions to return varchar

--- Requirement6.sql
+++ Requirement6.sql
@@ -5,10 +5,13 @@ DROP FUNCTION IF EXISTS dbo.fnCompanions;
 GO
 
 CREATE FUNCTION fnCompanions (@EpisodeId int)
-RETURNS TABLE AS
-RETURN
-	SELECT c.CompanionName
+RETURNS varchar(MAX) AS
+BEGIN
+	DECLARE @companions varchar(MAX);
+	SELECT @companions = CONCAT(@companions + ', ', c.CompanionName)
 	FROM tblCompanion c
 	INNER JOIN tblEpisodeCompanion ec ON ec.CompanionId = c.CompanionId
 	WHERE ec.EpisodeId = @EpisodeId
+	RETURN @companions;
+END;
 GO
