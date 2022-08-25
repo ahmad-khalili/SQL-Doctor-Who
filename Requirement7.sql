7d8b0d996b715574eecaefda858e8aeee495fc8c
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-25 13:19:00 +0300

    Modify fnEnemies to return varchar

--- Requirement7.sql
+++ Requirement7.sql
@@ -5,10 +5,13 @@ DROP FUNCTION IF EXISTS dbo.fnEnemies;
 GO
 
 CREATE FUNCTION fnEnemies (@EpisodeId int)
-RETURNS TABLE AS
-RETURN
-	SELECT e.EnemyName
+RETURNS varchar(MAX) AS
+BEGIN
+	DECLARE @enemies varchar(MAX);
+	SELECT @enemies = CONCAT(@enemies + ', ', e.EnemyName)
 	FROM tblEnemy e
 	INNER JOIN tblEpisodeEnemy ee ON ee.EnemyId = e.EnemyId
 	WHERE ee.EpisodeId = @EpisodeId
+	RETURN @enemies
+END;
 GO
